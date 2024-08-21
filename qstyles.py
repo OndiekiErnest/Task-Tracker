ae28308b03f3aa04081ef16b208aae49684d6551
Ernesto2018 <ernestondieki12@gmail.com>
2024-08-21 22:54:33 +0300

    Changed the appearance of QGroupBox

--- qstyles.py
+++ qstyles.py
@@ -6,4 +6,13 @@ font: 16px;
 font-family: Helvetica;
 letter-spacing: 1.5px;
 }
+QGroupBox {
+    border: 2px solid #e6e6e6;
+    border-radius: 2px;
+    margin-top: 4ex;
+}
+QGroupBox::title {
+    subcontrol-origin: margin;
+    subcontrol-position: top left;
+}
 """
