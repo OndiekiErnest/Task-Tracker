136288243ab6c6d14f8f8cab72bcea237105598a
Ernesto2018 <ernestondieki12@gmail.com>
2024-09-08 13:54:40 +0300

    Added animated button that slides in and out of position

---
+++ customwidgets/buttons.py
@@ -0,0 +1,48 @@
+"""custom PyQt6 buttons"""
+
+from PyQt6.QtCore import QPropertyAnimation, QRect, QEasingCurve
+from PyQt6.QtWidgets import QPushButton
+
+
+class InOutButton(QPushButton):
+    """button that slides into view and when hiding"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # initialize the animation
+        self.animation = QPropertyAnimation(self, b"geometry")
+
+    def show(self):
+        super().show()
+        self.slide_in()
+
+    def slide_in(self):
+        self.animation.setDuration(500)  # duration of the slide-in animation
+        self.animation.setStartValue(
+            QRect(
+                -100,
+                self.geometry().top(),
+                self.width(),
+                self.height(),
+            )
+        )
+        self.animation.setEndValue(
+            QRect(
+                self.geometry().left(),
+                self.geometry().top(),
+                self.width(),
+                self.height(),
+            )
+        )
+        self.animation.setEasingCurve(QEasingCurve.Type.OutCubic)
+        self.animation.start()
+
+    def slide_out(self):
+        self.animation.setDuration(500)  # duration of the slide-out animation
+        self.animation.setStartValue(self.geometry())
+        self.animation.setEndValue(
+            QRect(-100, self.geometry().top(), self.width(), self.height())
+        )
+        self.animation.setEasingCurve(QEasingCurve.Type.InCubic)
+        self.animation.start()
