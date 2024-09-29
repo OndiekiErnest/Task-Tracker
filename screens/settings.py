03bc9296348d6329db17e7b0051f44894b5c1fd7
Ernesto2018 <ernestondieki12@gmail.com>
2024-09-29 22:44:04 +0300

    Moved notification settings to its class

--- screens/settings.py
+++ screens/settings.py
@@ -1,7 +1,7 @@
 """widget for settings"""
 
 import logging
-from PyQt6.QtWidgets import QScrollArea, QWidget, QVBoxLayout, QHBoxLayout
+from PyQt6.QtWidgets import QScrollArea, QWidget, QGroupBox, QVBoxLayout, QHBoxLayout
 from PyQt6.QtCore import Qt
 from screens.topic_setter import TopicSetter
 from customwidgets.frames import Line
@@ -12,6 +12,44 @@ from datastructures.settings import settings
 
 
 logger = logging.getLogger(__name__)
+
+
+class NotificationsSettings(QGroupBox):
+    """notifications settings group"""
+
+    def __init__(self, **kwargs):
+        super().__init__("Notifications", **kwargs)
+
+        mainlayout = QVBoxLayout(self)
+        mainlayout.setSpacing(20)
+        mainlayout.setContentsMargins(8, 20, 8, 8)
+
+        remindlayout = QHBoxLayout()
+
+        self.remind_after = NamedLineEditV("Notify after every")
+        self.remind_after.child.setText(str(settings["notify_after"]))
+        self.remind_after.child.editingFinished.connect(self.remindAfterEdited)
+
+        self.remind_units = TimeUnits()
+        self.remind_units.setCurrentText(settings["notify_units"])
+        self.remind_units.currentTextChanged.connect(self.remindUnitsChanged)
+
+        remindlayout.addWidget(self.remind_after)
+        remindlayout.addWidget(self.remind_units)
+        # disable notifications
+        self.disable_notifications = DisableNotifs("Disable notifications")
+
+        mainlayout.addLayout(remindlayout)
+        mainlayout.addWidget(self.disable_notifications)
+
+    def remindAfterEdited(self):
+        """when remind after has been edited"""
+        tm = int(self.remind_after.child.text())
+        settings["notify_after"] = tm
+
+    def remindUnitsChanged(self, current: str):
+        """current units changed"""
+        settings["notify_units"] = current
 
 
 class SettingsWindow(QScrollArea):
@@ -31,42 +69,21 @@ class SettingsWindow(QScrollArea):
         layout.setContentsMargins(6, 20, 6, 20)
         layout.setSpacing(15)
         layout.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+
         self.setWidget(swidget)
 
         # widgets
         # set topic title, start, stop
         self.topic_adder = TopicSetter()
-        # remind after:
-        remindlayout = QHBoxLayout()
 
-        self.remind_after = NamedLineEditV("Notify after every")
-        self.remind_after.child.setText(str(settings["notify_after"]))
-        self.remind_after.child.editingFinished.connect(self.remindAfterEdited)
-
-        self.remind_units = TimeUnits()
-        self.remind_units.setCurrentText(settings["notify_units"])
-        self.remind_units.currentTextChanged.connect(self.remindUnitsChanged)
-
-        remindlayout.addWidget(self.remind_after)
-        remindlayout.addWidget(self.remind_units)
-        # disable notifications
-        self.disable_notifications = DisableNotifs("Disable notifications")
+        # notifications group
+        self.notifs_group = NotificationsSettings()
 
         layout.addWidget(self.topic_adder)
         layout.addWidget(Line())
-        layout.addLayout(remindlayout)
-        layout.addWidget(self.disable_notifications)
+        layout.addWidget(self.notifs_group)
 
     def setModel(self, model: TopicsModel):
         """set model for topics"""
         logger.info(f"Model set to '{model}'")
         self.topic_adder.setModel(model)
-
-    def remindAfterEdited(self):
-        """when remind after has been edited"""
-        tm = int(self.remind_after.child.text())
-        settings["notify_after"] = tm
-
-    def remindUnitsChanged(self, current: str):
-        """current units changed"""
-        settings["notify_units"] = current
