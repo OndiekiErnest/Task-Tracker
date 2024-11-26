e4c842dfc46e6865a474167b1e5f73747c55594b
Ernesto2018 <ernestondieki12@gmail.com>
2024-11-26 16:35:20 +0300

    Added problems delegate

--- customwidgets/delegates.py
+++ customwidgets/delegates.py
@@ -1,5 +1,6 @@
 """custom PyQt6 QStyledItemDelegate classes"""
 
+import logging
 from PyQt6.QtWidgets import (
     QStyledItemDelegate,
     QPlainTextEdit,
@@ -9,7 +10,9 @@ from PyQt6.QtWidgets import (
     QDateTimeEdit,
 )
 from PyQt6.QtCore import Qt, QRect, QModelIndex, QTime, QDateTime
-from PyQt6.QtGui import QPainter
+
+
+logger = logging.getLogger(__name__)
 
 
 class CommentsDelegate(QStyledItemDelegate):
@@ -34,18 +37,25 @@ class CommentsDelegate(QStyledItemDelegate):
     def setTopics(self, topics: list):
         """update topics list"""
         self._topics = topics
+        logger.info(f"Notes topics updated")
 
     def topicID(self, title: str):
         """get topic id from title"""
         for tpc in self._topics:
             if tpc.title == title:
+                logger.info(f"Found topic ID for {title!r}")
                 return tpc.topic_id
+
+        logger.info(f"Not Found: topic ID for {title!r}")
 
     def topicTitle(self, topic_id: int):
         """get topic title from id"""
         for tpc in self._topics:
             if tpc.topic_id == topic_id:
+                logger.info(f"Found topic title of ID {topic_id}")
                 return tpc.title
+
+        logger.info(f"Not Found: topic title of ID {topic_id}")
 
     # def paint(self, painter: QPainter, option, index: QModelIndex):
     #     # use topic id to get title for display
@@ -121,16 +131,19 @@ class CommentsDelegate(QStyledItemDelegate):
             qdatetime = editor.dateTime()
             datetime_str = qdatetime.toString("yyyy-MM-dd HH:mm:ss")
             model.setData(index, datetime_str, Qt.ItemDataRole.EditRole)
+            logger.info(f"Notes timestamp changed to: {datetime_str!r}")
 
         elif column == self.comment_col:
             # save data from QPlainTextEdit
             text = editor.toPlainText()
             model.setData(index, text, Qt.ItemDataRole.EditRole)
+            logger.info(f"Notes updated to: {text!r}")
 
         elif column == self.topic_col:
             tpc_title = editor.currentText()
             tpc_id = self.topicID(tpc_title)
             model.setData(index, tpc_id, Qt.ItemDataRole.EditRole)
+            logger.info(f"Notes topic changed to: {tpc_title!r}")
 
         else:
             super().setModelData(editor, model, index)
@@ -212,11 +225,80 @@ class TopicsDelegate(QStyledItemDelegate):
             time = editor.time()
             time_str = time.toString("HH:mm:ss")
             model.setData(index, time_str, Qt.ItemDataRole.EditRole)
+            logger.info(f"Topic start time changed to: {time!r}")
 
         elif column == self.int_col:
             # Save data from QSpinBox
             span_int = editor.value()
             model.setData(index, span_int, Qt.ItemDataRole.EditRole)
+            logger.info(f"Topic span changed to: {span_int}")
+
+        else:
+            super().setModelData(editor, model, index)
+
+    def updateEditorGeometry(self, editor, option, index: QModelIndex):
+        editor.setGeometry(option.rect)
+
+
+class ProblemsDelegate(QStyledItemDelegate):
+    """
+    delegate for the problems table,
+    uses: QComboBox for editing topics
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # variables
+        self.topics_col = 3
+        # topics
+        self._topics = []
+
+    def setTopics(self, topics: list):
+        """update topics list"""
+        self._topics = topics
+        logger.info(f"Problems topics updated")
+
+    def topicID(self, title: str):
+        """get topic id from title"""
+        for tpc in self._topics:
+            if tpc.title == title:
+                logger.info(f"Found topic ID for {title!r}")
+                return tpc.topic_id
+
+        logger.info(f"Not Found: topic ID for {title!r}")
+
+    def createEditor(self, parent, option, index: QModelIndex):
+        column = index.column()
+        if column == self.topics_col:
+            editor = QComboBox(parent)
+            editor.addItems(t.title for t in self._topics)
+
+        else:
+            # default to base class implementation for other columns
+            editor = super().createEditor(parent, option, index)
+
+        return editor
+
+    def setEditorData(self, editor, index: QModelIndex):
+        column = index.column()
+        value = index.data(Qt.ItemDataRole.EditRole)
+
+        if column == self.topics_col:
+            try:
+                editor.setCurrentText(value)
+            except TypeError:
+                # raised if an int is being set
+                pass
+        else:
+            super().setEditorData(editor, index)
+
+    def setModelData(self, editor, model, index: QModelIndex):
+        column = index.column()
+        if column == self.topics_col:
+            tpc_title = editor.currentText()
+            tpc_id = self.topicID(tpc_title)
+            model.setData(index, tpc_id, Qt.ItemDataRole.EditRole)
+            logger.info(f"Problems topic updated to: {tpc_title!r}")
         else:
             super().setModelData(editor, model, index)
 
