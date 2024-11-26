8b24e138accf34fea019249b835a61a0e0f7def3
Ernesto2018 <ernestondieki12@gmail.com>
2024-11-26 16:41:21 +0300

    Simplified the window because most of the code was moved to searchtables.py

--- screens/comments.py
+++ screens/comments.py
@@ -3,17 +3,10 @@
 import logging
 from PyQt6.QtWidgets import (
     QWidget,
-    QPushButton,
-    QLineEdit,
     QVBoxLayout,
-    QHBoxLayout,
 )
-from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QIcon
-from models import CommentsModel, SearchableModel
-from customwidgets.tableviews import CommentsTable
-from customwidgets.buttons import InOutButton
-from constants import ADDTOPIC_ICON, DELETE_ICON, SEARCH_ICON
+from models import CommentsModel
+from customwidgets.searchtables import CommentsTableview
 
 logger = logging.getLogger(__name__)
 
@@ -26,60 +19,18 @@ class CommentsWindow(QWidget):
 
         layout = QVBoxLayout(self)
 
-        toplayout = QHBoxLayout()
-        layout.addLayout(toplayout)
+        self.table_group = CommentsTableview("Notes")
 
-        self.model = SearchableModel()
-
-        self.delete_btn = InOutButton("Delete")
-        self.delete_btn.setIcon(QIcon(DELETE_ICON))
-        self.delete_btn.hide()
-
-        self.add_topic = QPushButton("Topic")
-        self.add_topic.setIcon(QIcon(ADDTOPIC_ICON))
-
-        self.add_record = QPushButton("Notes")
-        self.add_record.setIcon(QIcon(ADDTOPIC_ICON))
-
-        self.search_input = QLineEdit()
-        self.search_input.addAction(
-            QIcon(SEARCH_ICON), QLineEdit.ActionPosition.LeadingPosition
-        )
-        self.search_input.setPlaceholderText("Search entries...")
-        self.search_input.setClearButtonEnabled(True)
-        self.search_input.textChanged.connect(self.model.setFilterFixedString)
-
-        self.tableview = CommentsTable()
-        self.tableview.setModel(self.model)
-
-        # add buttons to layout
-        toplayout.addWidget(self.add_topic, alignment=Qt.AlignmentFlag.AlignLeft)
-        toplayout.addWidget(self.add_record)
-        toplayout.addWidget(self.delete_btn)
-        toplayout.addStretch()
-        toplayout.addWidget(self.search_input)
-
-        layout.addWidget(self.tableview)
+        layout.addWidget(self.table_group)
 
     def sRows(self):
-        return [
-            self.model.mapToSource(idx)
-            for idx in self.tableview.selectionModel().selectedRows()
-        ]
+        """return selected rows"""
+        return self.table_group.sRows()
 
     def setModel(self, model: CommentsModel):
         """set database model"""
         logger.info(f"Model set to '{model}'")
 
-        self.model.setSourceModel(model)
+        self.table_group.setModel(model)
 
-        self.tableview.hideColumn(model.fieldIndex("id"))
-        self.tableview.selectionModel().selectionChanged.connect(self.toggle_delete)
-
-    def toggle_delete(self):
-        """show or hide delete_btn"""
-        if self.sRows():
-            if not self.delete_btn.isVisible():
-                self.delete_btn.show()
-        else:
-            self.delete_btn.hide()
+        self.table_group.hideColumn(model.fieldIndex("id"))
