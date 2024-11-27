f92e4a39644f420936897e79293eb7d01fc76c87
Ernesto2018 <ernestondieki12@gmail.com>
2024-11-27 14:05:52 +0300

    New duration chooser

--- customwidgets/groupboxes/__init__.py
+++ customwidgets/groupboxes/__init__.py
@@ -2,10 +2,10 @@
 
 from .comboboxes import NamedCombobox
 from .checkboxes import NamedCheckbox
-from .lineedits import NamedLineEdit, NamedLineEditV
+from .lineedits import NamedLineEdit
 from .spinboxes import NamedSpinbox
 from .timeedits import NamedTimeEdit
 from .plaintextedits import NamedPlainTextEdit
-from .notifications import NotifsToggle
+from .notifications import NotifsToggle, DurationChooser
 from .problems import NewProblem
 from .topics import NewTopic
--- customwidgets/groupboxes/notifications.py
+++ customwidgets/groupboxes/notifications.py
@@ -5,9 +5,13 @@ from PyQt6.QtWidgets import (
     QGroupBox,
     QCheckBox,
     QHBoxLayout,
+    QLineEdit,
+    QComboBox,
 )
+from PyQt6.QtCore import QRegularExpression
+from PyQt6.QtGui import QRegularExpressionValidator
 from datastructures.settings import settings
-
+from constants import TIME_UNITS
 
 logger = logging.getLogger(__name__)
 
@@ -41,3 +45,49 @@ class NotifsToggle(QGroupBox):
     def onCheckSun(self, disabled: bool):
         settings["disable_sunday"] = disabled
         logger.info(f"Sunday disabled: {disabled}")
+
+
+class DurationChooser(QGroupBox):
+    """widget for specifying duration"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        mainlayout = QHBoxLayout(self)
+
+        self.duration = QLineEdit()
+        self.duration.setText(str(settings["notify_after"]))
+        self.duration.editingFinished.connect(self.notify_after_edited)
+
+        self.units = QComboBox()
+        self.units.addItems(TIME_UNITS.keys())
+        self.units.setCurrentText(settings["notify_units"])
+        self.units.currentTextChanged.connect(self.notify_units_changed)
+
+        validator = QRegularExpressionValidator(self)
+        validator.setRegularExpression(QRegularExpression("[0-9]+"))
+
+        self.duration.setValidator(validator)
+
+        mainlayout.addWidget(self.duration)
+        mainlayout.addWidget(self.units)
+
+    def set_units(self, unit: str):
+        self.units.setCurrentText(unit)
+
+    def set_duration(self, dur: str):
+        self.duration.setText(dur)
+
+    def minutes(self):
+        return int(self.duration.text()) * TIME_UNITS[self.units.currentText()]
+
+    def notify_after_edited(self):
+        """when notify after has been edited"""
+        tm = int(self.duration.text())
+        settings["notify_after"] = tm
+        logger.info(f"'notify_after' changed to: {tm}")
+
+    def notify_units_changed(self, current: str):
+        """current units changed"""
+        settings["notify_units"] = current
+        logger.info(f"'notify_units' changed to: {current}")
