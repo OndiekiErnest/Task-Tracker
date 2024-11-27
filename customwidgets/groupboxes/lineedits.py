b38f1f1d21ee2cbc6be4f88216d9d8c43de78a3a
Ernesto2018 <ernestondieki12@gmail.com>
2024-11-27 14:07:38 +0300

    Named line edit for integers integrated into duration chooser

--- customwidgets/groupboxes/lineedits.py
+++ customwidgets/groupboxes/lineedits.py
@@ -1,8 +1,6 @@
 """QLineEdit in a named QGroupBox"""
 
 from PyQt6.QtWidgets import QLineEdit
-from PyQt6.QtCore import QRegularExpression
-from PyQt6.QtGui import QRegularExpressionValidator
 from .base import NamedItem
 
 
@@ -14,15 +12,3 @@ class NamedLineEdit(NamedItem):
         self.child = QLineEdit()
 
         super().__init__(*args, **kwargs)
-
-
-class NamedLineEditV(NamedLineEdit):
-    """named QLineEdit with an int validator"""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        validator = QRegularExpressionValidator(self)
-
-        validator.setRegularExpression(QRegularExpression("[0-9]+"))
-        self.child.setValidator(validator)
